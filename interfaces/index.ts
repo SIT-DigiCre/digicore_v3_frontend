0eb083725c9472b2dd7dfbe61084ec9968184593
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-02-27 00:06:38 +0900

    Userの型定義を作成

---
+++ interfaces/index.ts
@@ -0,0 +1,21 @@
+export type User = {
+  id: string;
+  username: string;
+  studentNumber: string;
+  schoolGrade: number;
+  iconUrl: string;
+  discordUserId: string;
+  activeLimit: Date;
+  firstName: string;
+  lastName: string;
+  firstNameKana?: string;
+  lastNameKana?: string;
+  phoneNumber?: string;
+  address?: string;
+  parentName?: string;
+  parentCellPhoneNumber?: string;
+  parentHomePhoneNumber?: string;
+  parentAddress?: string;
+  self_introduction?: string;
+  shortSelfIntroduction?: string;
+};
