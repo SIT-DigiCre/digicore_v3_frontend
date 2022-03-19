7dccb2a1f32c41373311cb5b982df5939bf08d21
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-03-20 00:32:22 +0900

    サーバー側のログインAPIに接続

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,13 +1,13 @@
 import { Grid, Button, Container } from "@mui/material";
 import { Google } from "@mui/icons-material";
+import { baseURL } from "../utils/common";
 const LoginPage = () => {
-  const onClickLogin = () => {};
   return (
     <>
       <Container>
         <Grid xs={12} justifyItems="center">
           <Grid item xs={4}>
-            <Button variant="contained" startIcon={<Google />} onClick={onClickLogin}>
+            <Button variant="contained" startIcon={<Google />} href={baseURL + "/google/oauth/url"}>
               Googleログイン
             </Button>
           </Grid>
