61f5563d04fc49ab52467b81729cb2d78b6e58f4
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-04-12 01:53:15 +0900

    Discord連携の実装

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,5 +1,22 @@
+import { Button } from "@mui/material";
+import { useAuthState } from "../../hook/useAuthState";
+import { baseURL } from "../../utils/common";
+
 const ProfilePage = () => {
-  return <></>;
+  const { authState } = useAuthState();
+  return (
+    <>
+      {authState.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <Button href={baseURL + "/discord/oauth/url"} variant="contained">
+            {authState.user.discordUserId == "" ? "Discord連携" : "Discord再連携"}
+          </Button>
+        </>
+      )}
+    </>
+  );
 };
 
 export default ProfilePage;
