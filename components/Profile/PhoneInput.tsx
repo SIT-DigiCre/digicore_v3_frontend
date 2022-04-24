7cc46d4a4fa05cc825daa664b4753a6651912035
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-04-25 01:53:10 +0900

    電話番号入力を単純か

--- components/Profile/PhoneInput.tsx
+++ components/Profile/PhoneInput.tsx
@@ -8,47 +8,21 @@ type Props = {
   required?: boolean;
 };
 const PhoneInput = ({ title, onChange, initPhoneNumber, required }: Props) => {
-  const [num0, setNum0] = useState(initPhoneNumber ? initPhoneNumber.split("-")[0] : "");
-  const [num1, setNum1] = useState(initPhoneNumber ? initPhoneNumber.split("-")[1] : "");
-  const [num2, setNum2] = useState(initPhoneNumber ? initPhoneNumber.split("-")[2] : "");
+  const [num, setNum] = useState(initPhoneNumber ? initPhoneNumber : "");
 
   return (
     <>
       <h5>{title}</h5>
       <TextField
-        label="090"
+        label="09012345678"
         variant="outlined"
         required={required}
         margin="normal"
         type="number"
-        value={num0}
+        value={num}
         onChange={(e) => {
-          setNum0(e.target.value);
-          onChange(`${e.target.value}-${num1}-${num2}`);
-        }}
-      />
-      <TextField
-        label="1234"
-        variant="outlined"
-        required={required}
-        margin="normal"
-        type="number"
-        value={num1}
-        onChange={(e) => {
-          setNum1(e.target.value);
-          onChange(`${num0}-${e.target.value}-${num2}`);
-        }}
-      />
-      <TextField
-        label="5678"
-        variant="outlined"
-        required={required}
-        margin="normal"
-        type="number"
-        value={num2}
-        onChange={(e) => {
-          setNum2(e.target.value);
-          onChange(`${num0}-${num1}-${e.target.value}`);
+          setNum(e.target.value);
+          onChange(e.target.value);
         }}
       />
     </>
