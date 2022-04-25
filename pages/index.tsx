7dd85b7e46e563792b29dec7f3dcf10e35b42f81
Shimiiy <bangchaq2002@gmail.com>
2022-04-25 23:52:55 +0800

    カレンダーを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
+import { Container } from "@mui/material";
+
 const IndexPage = () => {
-  return <></>;
+  return (
+    <Container>
+      <h1>ようこそ、デジコア3.0へ</h1>
+      <p>デジコア3.0は開発途上のアプリケーションです。優しい気持ちで接してみてください</p>
+      <iframe
+        src="https://calendar.google.com/calendar/embed?src=sitdigicrecircle%40gmail.com&ctz=Asia%2FTokyo"
+        style={{ border: 0 }}
+        width="800"
+        height="600"
+        frameBorder="0"
+        scrolling="no"
+      ></iframe>
+    </Container>
+  );
 };
 
 export default IndexPage;
