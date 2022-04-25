5bff72bad6a5a5da9bd268a47219ee120f99c022
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-04-26 01:12:42 +0900

    レスポンシブ対応

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,16 @@ const IndexPage = () => {
     <Container>
       <h1>ようこそ、デジコア3.0へ</h1>
       <p>デジコア3.0は開発途上のアプリケーションです。優しい気持ちで接してみてください</p>
-      <iframe
-        src="https://calendar.google.com/calendar/embed?src=sitdigicrecircle%40gmail.com&ctz=Asia%2FTokyo"
-        style={{ border: 0 }}
-        width="800"
-        height="600"
-        frameBorder="0"
-        scrolling="no"
-      ></iframe>
+      <div className="index_calender">
+        <iframe
+          src="https://calendar.google.com/calendar/embed?src=sitdigicrecircle%40gmail.com&ctz=Asia%2FTokyo"
+          style={{ border: 0 }}
+          width="800"
+          height="600"
+          frameBorder="0"
+          scrolling="no"
+        ></iframe>
+      </div>
     </Container>
   );
 };
