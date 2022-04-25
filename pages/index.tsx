d9d509ff129d8a9e7fac811b7ecc0dbbc11380ee
Shimiiy <bangchaq2002@gmail.com>
2022-04-25 17:41:34 +0800

    indexからデジコアの登録ページへ飛べるよう編集

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import { Container, Link, Typography } from "@mui/material";
+
 const IndexPage = () => {
-  return <></>;
+  return (
+    <Container>
+      <Typography sx={{ m: 5 }} variant="h2">
+        デジコア v3.0 へようこそ。
+      </Typography>
+      <Typography sx={{ m: 5 }} variant="body1">
+        新規登録（入部）は
+        <Link href="register">こちら</Link>
+        で行えます。
+      </Typography>
+    </Container>
+  );
 };
 
 export default IndexPage;
