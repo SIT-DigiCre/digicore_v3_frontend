cc927230ca2f8e78e6bac9c537387ddee63b7db8
Shimiiy <bangchaq2002@gmail.com>
2022-04-28 13:26:22 +0800

    固定電話番号は必須ではないと明記

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -183,7 +183,7 @@ const ProfilePage = ({ registerMode }: Props) => {
                   <Grid>
                     <PhoneInput
                       onChange={onChangeParentHomePhone}
-                      title="固定電話番号"
+                      title="固定電話番号（非所持の方は無記入）"
                       initPhoneNumber={user.parentHomePhoneNumber}
                     />
                   </Grid>
