23dbd08cb22af7b2083d638872b6e4810e3d286a
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-07-02 15:46:20 +0900

    ErrorViewの実装

---
+++ components/Error/ErrorView.tsx
@@ -0,0 +1,24 @@
+import { Alert, AlertTitle } from "@mui/material";
+import { useErrorState } from "../../hook/useErrorState";
+
+const ErrorView = () => {
+  const { errors } = useErrorState();
+  return (
+    <>
+      {errors.length > 0 ? (
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          <ul>
+            {errors.map((e) => (
+              <li>{e.message}</li>
+            ))}
+          </ul>
+        </Alert>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default ErrorView;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import { RecoilRoot } from "recoil";
 import NavBar from "../components/Home/NavBar";
 import "../style/common.css";
 import "highlightjs/styles/vs2015.css";
+import ErrorView from "../components/Error/ErrorView";
 
 const App = ({ Component, pageProps }) => {
   return (
     <RecoilRoot>
       <NavBar />
+      <ErrorView />
       <Component {...pageProps} />
     </RecoilRoot>
   );
