483775f3e4d4aed44b7665d316b2f610e12ea2f0
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-07-27 19:21:50 +0900

    プロファイルの編集画面を作成

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,15 +1,9 @@
-import { Button, Container, Grid, TextField, Typography, Alert } from "@mui/material";
+import { Container } from "@mui/material";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
-import { ChangeEventHandler, useEffect, useState } from "react";
-import NameInput from "../../components/Profile/NameInput";
-import PhoneInput from "../../components/Profile/PhoneInput";
+import ProfileEditor from "../../components/Profile/ProfileEditor";
 import ProfileRegister from "../../components/Profile/ProfileRegister";
 import { useAuthState } from "../../hook/useAuthState";
-import { User } from "../../interfaces";
-import { convertUserPrivateFromUser, convertUserProfileFromUser } from "../../interfaces/api";
-import { axios } from "../../utils/axios";
-import { baseURL } from "../../utils/common";
 
 type Props = {
   registerMode: boolean;
@@ -24,7 +18,7 @@ const ProfilePage = ({ registerMode }: Props) => {
         <p>Loading...</p>
       ) : (
         <Container sx={{ mx: 5, my: 3 }}>
-          {registerMode ? <ProfileRegister registerMode={registerMode} /> : <></>}
+          {registerMode ? <ProfileRegister registerMode={registerMode} /> : <ProfileEditor />}
         </Container>
       )}
     </>
