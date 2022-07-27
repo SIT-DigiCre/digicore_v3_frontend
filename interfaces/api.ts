bfb8f662e506622a374a76755bf49d77e264bff7
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-07-27 19:56:17 +0900

    UserPrivateAPIDataResponseを追加

--- interfaces/api.ts
+++ interfaces/api.ts
@@ -54,6 +54,11 @@ export type UserPrivateAPIData = {
   parent_name: string;
   phone_number: string;
 };
+
+export type UserPrivateAPIDataResponse = {
+  privateProfile: UserPrivateAPIData;
+  error: string;
+};
 export const convertUserPrivateFromUser: (user: User) => UserPrivateAPIData = (user) => {
   const userPrivate: UserPrivateAPIData = {
     address: user.address,
