6ecd68317f84295ca41082794af20822c1e59db4
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-08-06 01:57:20 +0900

    faviconの指定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,16 @@ import NavBar from "../components/Home/NavBar";
 import "../style/common.css";
 import "highlightjs/styles/vs2015.css";
 import ErrorView from "../components/Error/ErrorView";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }) => {
   return (
     <RecoilRoot>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
+      </Head>
       <NavBar />
       <ErrorView />
       <Component {...pageProps} />
