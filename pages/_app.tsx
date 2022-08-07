1a00a906928d8314e1eb2648128ef769f6c709df
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-08-08 06:12:12 +0900

    デフォルトのフォントとしてNotoSansJPを導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,10 @@ const App = ({ Component, pageProps }) => {
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <NavBar />
       <ErrorView />
