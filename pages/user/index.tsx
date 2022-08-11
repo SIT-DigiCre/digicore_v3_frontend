c2fc88eb019537e237f4c49d1e6a13cf2698d242
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-08-11 22:45:56 +0900

    ユーザー一覧ページを作成

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,21 +1,44 @@
-import { Container } from "@mui/material";
-import { useState } from "react";
+import { Avatar, Button, Card, CardHeader, Container, Grid } from "@mui/material";
+import { useRouter } from "next/router";
 import { useUserProfiles } from "../../hook/user/useUserProfiles";
 
 const UserIndexPage = () => {
   const { userProfiles, requestMoreProfiles } = useUserProfiles();
+  const router = useRouter();
   return (
     <Container>
-      {userProfiles.map((userProfile) => (
-        <p>{userProfile.username}</p>
-      ))}
-      <button
-        onClick={() => {
-          requestMoreProfiles();
-        }}
-      >
-        update
-      </button>
+      <Grid>
+        <h1>ユーザー一覧</h1>
+        <hr />
+      </Grid>
+      <Grid>
+        {userProfiles.map((userProfile) => (
+          <Card
+            sx={{ width: "32%", display: "inline-block", m: 0.5 }}
+            onClick={() => {
+              router.push(`/user/${userProfile.id}`);
+            }}
+            className="clickable"
+            key={userProfile.id}
+          >
+            <CardHeader
+              avatar={<Avatar src={userProfile.icon_url} />}
+              title={userProfile.username}
+              subheader={userProfile.short_self_introduction}
+            ></CardHeader>
+          </Card>
+        ))}
+      </Grid>
+      <Grid sx={{ textAlign: "center" }}>
+        <Button
+          variant="contained"
+          onClick={() => {
+            requestMoreProfiles();
+          }}
+        >
+          Load More
+        </Button>
+      </Grid>
     </Container>
   );
 };
