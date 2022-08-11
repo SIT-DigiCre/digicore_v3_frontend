367d2dab9942217c950535c2178df8d73dee49f3
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-08-12 08:11:54 +0900

    ユーザーページを追加

---
+++ pages/user/[id].tsx
@@ -0,0 +1,60 @@
+import { Container } from "@mui/material";
+import { GetServerSideProps } from "next";
+import Breadcrumbs from "../../components/Common/Breadcrumb";
+import MarkdownView from "../../components/Common/MarkdownView";
+import { useIntroduction } from "../../hook/profile/useIntroduction";
+import { useProfile } from "../../hook/profile/useProfile";
+
+type Props = {
+  id: string;
+  error?: string;
+};
+const UserProfilePage = ({ id, error }: Props) => {
+  const profile = useProfile(id);
+  const introMd = useIntroduction(id);
+  if (!profile || introMd === undefined) return <p>Loading...</p>;
+  return (
+    <Container>
+      <Breadcrumbs
+        links={[
+          { text: "Home", href: "/" },
+          { text: "User", href: "/user" },
+          { text: profile.username },
+        ]}
+      />
+      <div style={{ marginTop: "10px" }}>
+        <div style={{ display: "inline-block", textAlign: "center", width: "30%" }}>
+          <img
+            src={profile.icon_url}
+            alt={profile.username + "のアイコン"}
+            style={{ maxWidth: "100%", borderRadius: "50%" }}
+          />
+        </div>
+        <div style={{ display: "inline-block", width: "70%", textAlign: "center" }}>
+          <div>
+            <h1>{profile.username}</h1>
+          </div>
+          <div>
+            <p>{profile.short_self_introduction}</p>
+          </div>
+          <div>
+            <p>学年: {profile.school_grade}</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <MarkdownView md={introMd} />
+      </div>
+    </Container>
+  );
+};
+export default UserProfilePage;
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  try {
+    const id = params?.id;
+    return { props: { id } };
+  } catch (error) {
+    return { props: { errors: error.message } };
+  }
+};
