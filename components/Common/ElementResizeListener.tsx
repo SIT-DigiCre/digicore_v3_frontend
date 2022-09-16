dc27a1850c5cb81b5c6c63e9de5aea8ba3a4f701
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-09-17 04:18:33 +0900

    ファイルを表示する処理を追加

---
+++ components/Common/ElementResizeListener.tsx
@@ -0,0 +1,60 @@
+import { useRef, useEffect, useCallback, RefObject } from "react";
+
+const ElementResizeListener = (props: { onResize: (event: Event) => void }) => {
+  const rafRef = useRef(0);
+  const objectRef: RefObject<HTMLObjectElement> = useRef(null);
+  const onResizeRef = useRef(props.onResize);
+
+  onResizeRef.current = props.onResize;
+
+  const _onResize = useCallback((e: Event) => {
+    if (rafRef.current) {
+      cancelAnimationFrame(rafRef.current);
+    }
+    rafRef.current = requestAnimationFrame(() => {
+      onResizeRef.current(e);
+    });
+  }, []);
+
+  const onLoad = useCallback(() => {
+    const obj = objectRef.current;
+    if (obj && obj.contentDocument && obj.contentDocument.defaultView) {
+      // defaultViewはDocumentに関連付けられているWindowオブジェクトを返す
+      // object要素のWindowオブジェクトに対し、リサイズイベントを登録する
+      obj.contentDocument.defaultView.addEventListener("resize", _onResize);
+    }
+  }, []);
+
+  useEffect(() => {
+    // クリーンアップ処理
+    return () => {
+      const obj = objectRef.current;
+      if (obj && obj.contentDocument && obj.contentDocument.defaultView) {
+        obj.contentDocument.defaultView.removeEventListener("resize", _onResize);
+      }
+    };
+  }, []);
+
+  return (
+    <object
+      onLoad={onLoad}
+      ref={objectRef}
+      tabIndex={-1}
+      type={"text/html"}
+      data={"about:blank"}
+      title={""}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: "100%",
+        width: "100%",
+        pointerEvents: "none",
+        zIndex: -1,
+        opacity: 0,
+      }}
+    />
+  );
+};
+
+export default ElementResizeListener;
---
+++ components/File/FileView.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useRef, useState } from "react";
+import { FileObject, getFileKind } from "../../interfaces/file";
+import ElementResizeListener from "../Common/ElementResizeListener";
+
+type Props = {
+  file: FileObject;
+  width?: number;
+};
+const FileView = ({ file, width }: Props) => {
+  const contentRef: React.RefObject<HTMLDivElement> = useRef(null);
+  const [contentWidth, setContentWidth] = useState(100);
+  const [contentHight, setContentHight] = useState(100);
+  const onResize = (event: Event) => {
+    console.log(contentRef.current.getBoundingClientRect().width);
+    setContentWidth(contentRef.current.getBoundingClientRect().width);
+    setContentHight((contentRef.current.getBoundingClientRect().width * 10) / 16);
+  };
+
+  useEffect(() => {
+    setContentWidth(contentRef.current.getBoundingClientRect().width);
+    setContentHight((contentRef.current.getBoundingClientRect().width * 10) / 16);
+  }, []);
+  const getFileTag = () => {
+    switch (getFileKind(file.extension)) {
+      case "image":
+        return <img src={file.url} style={{ maxWidth: "100%" }} />;
+      case "docx":
+      case "xlsx":
+      case "pptx":
+        return (
+          <iframe
+            src={"https://view.officeapps.live.com/op/embed.aspx?src=" + file.url}
+            width={width ? width : contentWidth}
+            height={width ? (width * 10) / 16 : contentHight}
+            frameBorder="0"
+          />
+        );
+      case "video":
+        return <video src={file.url} style={{ maxWidth: "100%" }} controls />;
+      case "audio":
+        return (
+          <>
+            <p>{file.name}</p>
+            <audio src={file.url} controls></audio>
+          </>
+        );
+      case "exe":
+        return (
+          <>
+            <a href={file.url}>{file.name}</a>
+            <p>注意！実行形式ファイル</p>
+          </>
+        );
+      default:
+        return <a href={file.url}>{file.name}</a>;
+    }
+  };
+  return (
+    <div ref={contentRef} style={{ width: "100%" }}>
+      <ElementResizeListener onResize={onResize} />
+      {getFileTag()}
+    </div>
+  );
+};
+
+export default FileView;
