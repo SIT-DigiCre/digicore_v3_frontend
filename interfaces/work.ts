30a6673a4d65b823fd9c80ceaaab5cf091933521
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-09-16 16:36:38 +0900

    型定義

---
+++ interfaces/work.ts
@@ -0,0 +1,24 @@
+export type WorkAuther = {
+  id: string;
+  name: string;
+  icon_url: string;
+};
+export type WorkTag = {
+  id: string;
+  name: string;
+};
+export type Work = {
+  id: string;
+  name: string;
+  authers: WorkAuther[];
+  tags: WorkTag[];
+};
+export type WorkFile = {
+  id: string;
+  name: string;
+};
+export type WorkDetail = Work & {
+  description: string;
+  files: WorkFile[];
+};
+export type WorkTagDetail = WorkTag & {};
