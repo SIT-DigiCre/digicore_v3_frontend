992fe9b40a4a0a21edabec961084c42d6a671bb8
Chisen Kaoru <cordx56@cordx.net>
2022-10-16 11:29:21 +0900

    Bredcrumbを修正

--- components/Common/Breadcrumb.tsx
+++ components/Common/Breadcrumb.tsx
@@ -8,7 +8,7 @@ const Breadcrumbs = ({ links }: Props) => {
   return (
     <MBC>
       {links.map((link) => (
-        <div key={link.href}>
+        <div key={link.text}>
           {link.href ? (
             <Link href={link.href}>
               <p className="breadcrumbs-link">{link.text}</p>
