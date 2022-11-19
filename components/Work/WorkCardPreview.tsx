c5c10da7ae4c80dd4f9c7be4f11e9a17d8e2ad74
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-11-19 15:51:11 +0900

    lengthが0の時の条件を追加

--- components/Work/WorkCardPreview.tsx
+++ components/Work/WorkCardPreview.tsx
@@ -8,6 +8,7 @@ type Props = {
 export const WorkCardPreview = ({ id }: Props) => {
   const { workDetail } = useWork(id);
   if (!workDetail) return <p>Loading...</p>;
-  if (!workDetail.files) return <MarkdownView md={workDetail.description.substring(0, 120)} />;
+  if (!workDetail.files || workDetail.files.length === 0)
+    return <MarkdownView md={workDetail.description.substring(0, 120)} />;
   return <WorkFileView fileId={workDetail.files[0].fileId} />;
 };
