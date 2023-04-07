8101856761c3508947b5067a7c54a9ae8e917b73
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2023-04-08 04:16:52 +0900

    会計確認画面を修正

--- pages/payment/index.tsx
+++ pages/payment/index.tsx
@@ -1,8 +1,33 @@
-import { Box, Button, Checkbox, Container, Grid, Modal, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  Grid,
+  Modal,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import { usePayments } from "../../hook/payment/usePayments";
 import { Payment } from "../../interfaces/payment";
-
+const modalStyle = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 const PaymentPage = () => {
   const [payments, updatePayments] = usePayments();
   const [targetPayment, updateTargetPayment] = useState<Payment>();
@@ -31,13 +56,21 @@ const PaymentPage = () => {
                   <TableCell>{payment.studentNumber}</TableCell>
                   <TableCell>{payment.transferName}</TableCell>
                   <TableCell>
-                    <Checkbox
-                      checked={payment.checked}
-                    ></Checkbox>
+                    {payment.checked ? (
+                      <span style={{ color: "green" }}>済</span>
+                    ) : (
+                      <span style={{ color: "red" }}>未</span>
+                    )}
                   </TableCell>
                   <TableCell>{payment.note}</TableCell>
                   <TableCell>
-                    <Button onClick={() => { updateTargetPayment(payment) }}>詳細</Button>
+                    <Button
+                      onClick={() => {
+                        updateTargetPayment(payment);
+                      }}
+                    >
+                      詳細
+                    </Button>
                   </TableCell>
                 </TableRow>
               ))}
@@ -46,27 +79,53 @@ const PaymentPage = () => {
         </TableContainer>
         <Modal
           open={targetPayment !== undefined}
-          onClose={() => { updateTargetPayment(undefined) }}
+          onClose={() => {
+            updateTargetPayment(undefined);
+          }}
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-          <Box className="payment-modal">
+          <Box sx={modalStyle}>
             <Typography id="modal-modal-title" variant="h6" component="h2">
               支払い詳細
             </Typography>
             <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              支払い番号: {targetPayment?.paymentId}<br />
-              学籍番号: {targetPayment?.studentNumber}<br />
-              支払い名義: {targetPayment?.transferName}<br />
-              確認: <Checkbox
+              支払い番号: {targetPayment?.paymentId}
+              <br />
+              学籍番号: {targetPayment?.studentNumber}
+              <br />
+              支払い名義: {targetPayment?.transferName}
+              <br />
+              確認:{" "}
+              <Checkbox
                 checked={targetPayment?.checked}
-                onChange={() => { updateTargetPayment({ ...targetPayment, checked: !targetPayment?.checked }) }}
+                onChange={() => {
+                  updateTargetPayment({ ...targetPayment, checked: !targetPayment?.checked });
+                }}
               ></Checkbox>
               <br />
-              備考: <TextField fullWidth value={targetPayment?.note} onChange={(e) => { updateTargetPayment({ ...targetPayment, note: e.target.value }) }}
+              備考:{" "}
+              <TextField
+                fullWidth
+                value={targetPayment?.note}
+                onChange={(e) => {
+                  updateTargetPayment({ ...targetPayment, note: e.target.value });
+                }}
               />
               <br />
-              <Button onClick={() => { updatePayments(targetPayment.paymentId, targetPayment.checked, targetPayment.note); updateTargetPayment(undefined) }}>保存</Button>
+              <Button
+                onClick={() => {
+                  updatePayments(
+                    targetPayment.paymentId,
+                    targetPayment.checked,
+                    targetPayment.note,
+                  );
+                  updateTargetPayment(undefined);
+                }}
+                variant="contained"
+              >
+                保存
+              </Button>
             </Typography>
           </Box>
         </Modal>
