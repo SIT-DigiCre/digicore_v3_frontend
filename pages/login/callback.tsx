dc3ca299e1ce39190bc394f5665f4e44fc889ab4
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2023-06-03 18:30:56 +0900

    フォームへの誘導を追記

--- pages/login/callback.tsx
+++ pages/login/callback.tsx
@@ -40,7 +40,16 @@ const LoginCallbackPage = ({ code }: Props) => {
         ログイン処理中... （この画面が長時間出る場合はログインからやり直して下さい）
       </p>
     );
-  return <p style={centerTextStyle}>ログイン失敗</p>;
+  return (
+    <div style={centerTextStyle}>
+      <p>ログイン失敗</p>
+      <br />
+      <p>何度か試行しても解決できない場合はインフラサポートフォームをご利用ください</p>
+      <a href="https://forms.gle/MQicA1No8LSZPe5QA" target="_blank" rel="noopener">
+        サポートフォームを開く
+      </a>
+    </div>
+  );
 };
 export const getServerSideProps: GetServerSideProps = async ({ params, query }) => {
   try {
