8d0753333a22579d5ec90a70423c978327626cdb
newt <ysti2019.win2020.st@gmail.com>
2025-06-06 22:25:34 +0900

    feat: users table

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ const eslintConfig = [
   eslintConfigPrettier,
   {
     rules: {
+      "react-hooks/exhaustive-deps": "off",
       "@next/next/no-img-element": "off",
       "no-console": [
         "error",
--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,35 +1,61 @@
-import { useRouter } from "next/router";
-
-import { Avatar, Card, CardHeader, Grid } from "@mui/material";
+import {
+  Avatar,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 import PageHead from "../../components/Common/PageHead";
 import { useUserProfiles } from "../../hook/user/useUserProfiles";
 
+const gradeMap = {
+  1: "学部1年",
+  2: "学部2年",
+  3: "学部3年",
+  4: "学部4年",
+  5: "修士1年",
+  6: "修士2年",
+  7: "博士1年",
+  8: "博士2年",
+  9: "博士3年",
+};
+
 const UserIndexPage = () => {
-  const { userProfiles, requestMoreProfiles } = useUserProfiles();
-  const router = useRouter();
+  const { userProfiles } = useUserProfiles();
 
   return (
     <>
       <PageHead title="部員一覧" />
-
-      {userProfiles.map((userProfile) => (
-        <Grid size={[12, 6, 4]} sx={{ padding: 0.5 }} key={userProfile.userId}>
-          <Card
-            onClick={() => {
-              router.push(`/user/${userProfile.userId}`);
-            }}
-            className="clickable"
-            key={userProfile.userId}
-          >
-            <CardHeader
-              avatar={<Avatar src={userProfile.iconUrl} />}
-              title={userProfile.username}
-              subheader={userProfile.shortIntroduction}
-            ></CardHeader>
-          </Card>
-        </Grid>
-      ))}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell>名前</TableCell>
+              <TableCell>学年</TableCell>
+              <TableCell>学籍番号</TableCell>
+              <TableCell>自己紹介</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {userProfiles.map((userProfile) => (
+              <TableRow key={userProfile.userId}>
+                <TableCell>
+                  <Avatar src={userProfile.iconUrl} sx={{ width: 40, height: 40 }} />
+                </TableCell>
+                <TableCell>{userProfile.username}</TableCell>
+                <TableCell>{gradeMap[userProfile.schoolGrade]}</TableCell>
+                <TableCell>{userProfile.studentNumber}</TableCell>
+                <TableCell>{userProfile.shortIntroduction}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
