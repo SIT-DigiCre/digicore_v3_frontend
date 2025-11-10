bf886ffea60dea971cefff74640307d31c4f2ecd
newt <ysti2019.win2020.st@gmail.com>
2025-11-10 13:20:42 +0900

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,37 @@
-import { FlatCompat } from '@eslint/eslintrc';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig } from 'eslint/config';
+import tsESLint from 'typescript-eslint';
 
-const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-})
-
-const eslintConfig = [
+const eslintConfig = defineConfig([
+  // 先頭のconfigオブジェクトでignoresのみを指定することでglobal ignoreになる
   {
-    ignores: ["node_modules/", ".next/"],
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   eslintConfigPrettier,
+  ...tsESLint.configs.recommended,
   {
+    files: ["**/*.{ts,tsx}"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      import: importPlugin,
+    },
     rules: {
       "react-hooks/exhaustive-deps": "off",
-      "@next/next/no-img-element": "off",
       "no-console": [
         "error",
         {
@@ -50,6 +66,6 @@ const eslintConfig = [
       ]
     },
   },
-];
+]);
 
 export default eslintConfig;
