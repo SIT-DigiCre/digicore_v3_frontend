739dbd59ca6710c420eba99ad659fc35f751c666
glyzinieh <contact@wisteriatp.work>
2026-04-12 12:25:15 +0900

    favicon.svgにリネーム

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           <link rel="icon" href="/favicon.ico" sizes="32x32" />
-          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         </Head>
         <body>
