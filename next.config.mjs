442af993ae4a29a233f2db697635e3d7c6b87080
Moritomo174 <250947871+Moritomo174@users.noreply.github.com>
2026-05-29 11:28:54 +0900

    fix: 開発モード時にpwaを無効化

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: false,
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
